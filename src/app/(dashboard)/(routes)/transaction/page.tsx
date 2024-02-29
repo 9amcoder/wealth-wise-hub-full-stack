8039a34cab7c41f21a8cd4c77f1b24fa17c98699
Quynh Dinh <erisdinh@gmail.com>
2024-02-28 21:31:32 -0500

    Dialogs in Transaction Page (#5)
    * Update README.md
    
    * Transaction Page UI
    
    * Add action buttons for rows and fix size of transaction tables in transaction page UI
    
    * Add dialogs in transaction page for transaction modification and deletion
    
    * Update package.json
    
    ---------
    
    Co-authored-by: 9amCoder <59545545+9amcoder@users.noreply.github.com>

--- src/app/(dashboard)/(routes)/transaction/page.tsx
+++ src/app/(dashboard)/(routes)/transaction/page.tsx
@@ -10,18 +10,31 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription, DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import {Label} from "@/components/ui/label";
+import { Input } from "@/components/ui/input"
 import Image from "next/image";
-import { PreviewTransaction, previewColumns } from "@/components/dashboard/transaction/preview-columns";
-import { Transaction, transactionColumns } from "@/components/dashboard/transaction/transaction-columns";
+import { previewColumns } from "@/components/dashboard/transaction/preview-columns";
+import { transactionColumns } from "@/components/dashboard/transaction/transaction-columns";
 import { PreviewDataTable } from "@/components/dashboard/transaction/preview-data-table";
 import { TransactionDataTable } from "@/components/dashboard/transaction/transaction-data-table";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import {DEFAULT_SEGMENT_KEY} from "next/dist/shared/lib/segment";
+
 import { useUser } from "@clerk/nextjs";
 
 interface TransactionPageProps {}
 
 const TransactionPage: React.FC<TransactionPageProps> = () => {
-
     const [selectedFile, setSelectedFile] = useState<string | null>(null);
     const { user, isLoaded } = useUser();
 
@@ -72,7 +85,7 @@ const TransactionPage: React.FC<TransactionPageProps> = () => {
 
     if (!data || (Array.isArray(data) && data.length === 0)) {
         return <p>No transaction data</p>;
-      }
+    }
 
     return (
         <div className="grid gap-3 m-[2]">
@@ -101,14 +114,62 @@ const TransactionPage: React.FC<TransactionPageProps> = () => {
                                         )}
                                     </div>
                                 </div>
+                                <Button className="text-[#282458] mt-2" variant="outline" type="button"
+                                        disabled={!selectedFile}>Extract</Button>
                             </form>
                         </CardContent>
                     </Card>
                 </div>
                 <div className="col-span-2">
-                    <Card>
+                    <Card className="h-[447px]">
                         <CardHeader>
-                            <CardTitle className="text-[#282458]">Preview</CardTitle>
+                            <div className="grid grid-cols-2">
+                                <CardTitle className="text-[#282458] col-span-1">Preview</CardTitle>
+                                <div className="col-span-1 top-0 right-0">
+                                    <Dialog>
+                                        <DialogTrigger asChild>
+                                            <div className="right-0 top-0">
+                                                <Button className="text-[#282458]" variant="outline" type="button">Add
+                                                    transaction</Button>
+                                            </div>
+                                        </DialogTrigger>
+                                        <DialogContent className="sm:max-w-[425px]">
+                                            <DialogHeader>
+                                                <DialogTitle className="text-[#282458]">Add new transaction</DialogTitle>
+                                                <DialogDescription>
+                                                    Enter information for new transaction here. Click save when you're done.
+                                                </DialogDescription>
+                                            </DialogHeader>
+                                            <div className="grid gap-4 py-4">
+                                                <div className="grid grid-cols-4 items-center gap-4">
+                                                    <Label htmlFor="transactionDate" className="text-right text-[#282458]">
+                                                        Date Time
+                                                    </Label>
+                                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                                        <DateTimePicker className="w-full h-full bg-transparent"
+                                                                        views={['year', 'day', 'hours', 'minutes', 'seconds']}/>
+                                                    </LocalizationProvider>
+                                                </div>
+                                                <div className="grid grid-cols-4 items-center gap-4">
+                                                    <Label htmlFor="title" className="text-right text-[#282458]">
+                                                        Item
+                                                    </Label>
+                                                    <Input id="title" value="" className="col-span-3"/>
+                                                </div>
+                                                <div className="grid grid-cols-4 items-center gap-4">
+                                                    <Label htmlFor="amount" className="text-right text-[#282458]">
+                                                        Amount
+                                                    </Label>
+                                                    <Input id="amount" value="" className="col-span-3"/>
+                                                </div>
+                                            </div>
+                                            <DialogFooter>
+                                                <Button className="text-[#282458] bg-[#F3F3FC] hover:bg-[#E5E5FF]" type="submit">Save</Button>
+                                            </DialogFooter>
+                                        </DialogContent>
+                                    </Dialog>
+                                </div>
+                            </div>
                         </CardHeader>
                         <CardContent>
                             <PreviewDataTable columns={previewColumns} data={data}/>
--- src/components/dashboard/transaction/preview-columns.tsx
+++ src/components/dashboard/transaction/preview-columns.tsx
@@ -2,7 +2,6 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { MoreHorizontal } from "lucide-react"
-
 import { Button } from "@/components/ui/button"
 import {
     DropdownMenu,
@@ -12,6 +11,16 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription, DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -58,8 +67,59 @@ export const previewColumns: ColumnDef<PreviewTransaction>[] = [
                     </DropdownMenuTrigger>
                     <DropdownMenuContent align="end">
                         <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem>Modify transaction details</DropdownMenuItem>
-                        <DropdownMenuItem>Delete transaction</DropdownMenuItem>
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>Modify transaction details</DropdownMenuItem>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-[425px]">
+                                <DialogHeader>
+                                    <DialogTitle className="text-[#282458]">Edit transaction</DialogTitle>
+                                    <DialogDescription>
+                                        Make changes to the transaction here. Click save when you're done.
+                                    </DialogDescription>
+                                </DialogHeader>
+                                <div className="grid gap-4 py-4">
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="transactionDate" className="text-right text-[#282458]">
+                                          Date Time
+                                        </Label>
+                                        <Input id="transactionDate" value={transaction.transactionDate}
+                                             className="col-span-3"/>
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="title" className="text-right text-[#282458]">
+                                          Item
+                                        </Label>
+                                        <Input id="title" value={transaction.title} className="col-span-3"/>
+                                    </div>
+                                    <div className="grid grid-cols-4 items-center gap-4">
+                                        <Label htmlFor="amount" className="text-right text-[#282458]">
+                                          Amount
+                                        </Label>
+                                        <Input id="amount" value={transaction.amount} className="col-span-3"/>
+                                    </div>
+                                </div>
+                            <DialogFooter>
+                                <Button className="text-[#282458] bg-[#F3F3FC] hover:bg-[#E5E5FF]" type="submit">Save changes</Button>
+                            </DialogFooter>
+                            </DialogContent>
+                        </Dialog>
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>Delete transaction</DropdownMenuItem>
+                            </DialogTrigger>
+                            <DialogContent>
+                                <DialogHeader>
+                                    <DialogTitle className="text-[#282458]">Are you sure?</DialogTitle>
+                                    <DialogDescription>
+                                        This will permanently remove the transaction from preview table.
+                                    </DialogDescription>
+                                    <DialogFooter>
+                                        <Button className="text-[#282458] bg-[#F3F3FC] hover:bg-[#E5E5FF]" type="submit">Confirm</Button>
+                                    </DialogFooter>
+                                </DialogHeader>
+                            </DialogContent>
+                        </Dialog>
                     </DropdownMenuContent>
                 </DropdownMenu>
             )
