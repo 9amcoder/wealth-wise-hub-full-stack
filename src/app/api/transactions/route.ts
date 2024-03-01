30b29f19d26604e00a16c0ebc7992166a8621b74
Steve Sultan <SteveSultan@outlook.com>
2024-02-29 21:33:55 -0500

    Refactor transaction route and add POST endpoint

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -1,13 +1,68 @@
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const prisma = new PrismaClient();
 
-export async function GET(){
-    try {
-        const transactions = await prisma.transaction.findMany();
-        return Response.json(transactions);
-    } catch (error) {
-        console.error(error);
-        return Response.error();
+export async function GET() {
+  try {
+    const transactions = await prisma.transaction.findMany();
+    return Response.json(transactions);
+  } catch (error) {
+    console.error(error);
+    return Response.error();
+  }
+}
+
+const transactionSchema = z.object({
+  userId: z.string(),
+  title: z.string(),
+  transactionDate: z.string().optional(),
+  amount: z.number(),
+});
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const payload = await req.json();
+    // if user not fond from prisma then return 404
+    const user = await prisma.user.findUnique({
+      where: {
+        clerkUserId: payload.userId,
+      },
+    });
+
+    if (!user) {
+      return Response.json(
+        {
+          message: "User not found",
+        },
+        { status: 404 }
+      );
     }
+
+    const response = transactionSchema.safeParse(payload);
+
+    if (!response.success) {
+      const [errors] = response.error.errors;
+      return Response.json(
+        {
+          message: "Invalid request",
+          error: errors.message,
+        },
+        { status: 400 }
+      );
+    }
+
+    const transaction = await prisma.transaction.create({
+      data: {
+        userId: payload.userId,
+        title: payload.title,
+        transactionDate: payload.transactionDate,
+        amount: payload.amount,
+      },
+    });
+    return Response.json(transaction);
+  } catch (error) {
+    console.error(error);
+    return Response.error();
+  }
 }
