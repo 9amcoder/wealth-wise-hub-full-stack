858176888330cd3b4655ed575b9f40e67294c987
Steve Sultan <SteveSultan@outlook.com>
2024-03-07 00:08:39 -0500

    Add transaction store and related functions as hook

---
+++ src/store/transactionStore.ts
@@ -0,0 +1,114 @@
+import handleApiError from "@/config/apiErrorHandler";
+import { get, post, put, remove } from "@/config/axiosConfig";
+import { Transaction } from "@prisma/client";
+import { AxiosError } from "axios";
+import { create } from "zustand";
+
+interface TransactionStore {
+  transactions: Transaction[];
+  transactionDataById: Transaction | null;
+  transactionsByUserId: Transaction[];
+  loading: boolean;
+  transactionError: string | null;
+  getTransactions: () => Promise<void>;
+  getTransactionById: (id: string) => Promise<void>;
+  getTransactionByUserId: (userId: string) => Promise<void>;
+  addTransaction: (transaction: Transaction) => Promise<void>;
+  updateTransaction: (transaction: Transaction) => Promise<void>;
+  deleteTransaction: (id: string) => Promise<void>;
+}
+
+const useTransactionStore = create<TransactionStore>((set) => ({
+  transactions: [],
+  transactionDataById: null,
+  transactionsByUserId: [],
+  loading: false,
+  transactionError: null,
+  getTransactions: async () => {
+    set({ transactionError: null, loading: true });
+    try {
+      const response = await get("/transactions");
+      set({ transactions: response.data });
+    } catch (transactionError) {
+      const errorMessage = handleApiError(transactionError as AxiosError);
+      set({ transactionError: errorMessage });
+      throw new Error(errorMessage);
+    } finally {
+      set({ loading: false });
+    }
+  },
+  getTransactionById: async (id) => {
+    set({ transactionError: null, loading: true });
+    try {
+      const response = await get(`/transaction/${id}`);
+      set({ transactionDataById: response.data });
+    } catch (transactionError) {
+      const errorMessage = handleApiError(transactionError as AxiosError);
+      set({ transactionError: errorMessage });
+      throw new Error(errorMessage);
+    } finally {
+      set({ loading: false });
+    }
+  },
+  getTransactionByUserId: async (userId) => {
+    set({ transactionError: null, loading: true });
+    try {
+        console.log("user id",userId);
+      const response = await get(`/transactions/${userId}`);
+      set({ transactionsByUserId: response.data });
+    } catch (transactionError) {
+      const errorMessage = handleApiError(transactionError as AxiosError);
+      set({ transactionError: errorMessage });
+      throw new Error(errorMessage);
+    } finally {
+      set({ loading: false });
+    }
+  },
+  addTransaction: async (transaction) => {
+    set({ transactionError: null, loading: true });
+    try {
+      const response = await post("/transactions", transaction);
+      set({ transactions: [...response.data, transaction] });
+    } catch (transactionError) {
+      const errorMessage = handleApiError(transactionError as AxiosError);
+      set({ transactionError: errorMessage });
+      throw new Error(errorMessage);
+    } finally {
+      set({ loading: false });
+    }
+  },
+  updateTransaction: async (transaction) => {
+    set({ transactionError: null, loading: true });
+    try {
+      const response = await put(
+        `/transactions/${transaction.id}`,
+        transaction
+      );
+      const updatedTransactions = response.data;
+      set({ transactions: updatedTransactions });
+    } catch (transactionError) {
+      const errorMessage = handleApiError(transactionError as AxiosError);
+      set({ transactionError: errorMessage });
+      throw new Error(errorMessage);
+    } finally {
+      set({ loading: false });
+    }
+  },
+  deleteTransaction: async (id) => {
+    set({ transactionError: null, loading: true });
+    try {
+      await remove(`/transactions/${id}`);
+      set((state) => ({
+        transactions: state.transactions.filter((t) => t.id !== id),
+      }));
+    } catch (transactionError) {
+      const errorMessage = handleApiError(transactionError as AxiosError);
+      set({ transactionError: errorMessage });
+      throw new Error(errorMessage);
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
+
+export default useTransactionStore;
