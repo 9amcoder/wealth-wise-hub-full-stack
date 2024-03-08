0c50bdaf6db0c1f161f97752b19a59dedb4dad0b
Quynh Dinh <erisdinh@gmail.com>
2024-03-07 19:07:53 -0500

    Modifed POST request endpoint for goal and balance

--- src/app/api/balance/route.ts
+++ src/app/api/balance/route.ts
@@ -1,18 +1,54 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+const balanceSchema = z.object({
+    balance: z.number(),
+    userId: z.string()
+});
+
+export async function POST(req: Request, res: Response) {
     try {
-        const { userId, balance } = req.body
-        const result = await prisma.balanceHistory.create({
+        const payload = await req.json();
+
+        const user = await prisma.user.findUnique({
+            where: {
+              clerkUserId: payload.userId,
+            },
+        });
+
+        if (!user) {
+            return Response.json(
+                {
+                    message: "User not found",
+                },
+                { 
+                    status: 404 
+                }
+            );
+        }
+
+        const response = balanceSchema.safeParse(payload);
+
+        if (!response.success) {
+            const [errors] = response.error.errors;
+            return Response.json(
+              {
+                message: "Invalid request",
+                error: errors.message,
+              },
+              { status: 400 }
+            );
+        }
+
+        const balance = await prisma.balanceHistory.create({
             data: {
-                balance: balance,
-                userId: userId
+                balance: payload.balance,
+                userId: payload.userId
             }
         })
-        return res.status(201).json(result)
+        return Response.json(balance);
     } catch (error) {
         console.error(error);
         return Response.error();
--- src/app/api/goals/route.ts
+++ src/app/api/goals/route.ts
@@ -1,19 +1,57 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+const goalSchema = z.object({
+    goalName: z.string(),
+    goalAmount: z.number(),
+    goalDate: z.date(),
+    userId: z.string()
+  });
+
+export async function POST(req: Request, res: Response) {
     try {
-        const { goalAmount, goalDate, userId } = req.body
-        const result = await prisma.goalHistory.create({
+        const payload = await req.json();
+
+        const user = await prisma.user.findUnique({
+            where: {
+              clerkUserId: payload.userId,
+            },
+        });
+
+        if (!user) {
+            return Response.json(
+                {
+                    message: "User not found",
+                },
+                { 
+                    status: 404 
+                }
+            );
+        }
+
+        const response = goalSchema.safeParse(payload);
+
+        if (!response.success) {
+            const [errors] = response.error.errors;
+            return Response.json(
+              {
+                message: "Invalid request",
+                error: errors.message,
+              },
+              { status: 400 }
+            );
+        }
+
+        const goal = await prisma.goalHistory.create({
             data: {
-                goalAmount: goalAmount,
-                goalDate: goalDate,
-                userId: userId
+                goalAmount: payload.goalAmount,
+                goalDate: payload.goalDate,
+                userId: payload.userId
             }
         })
-        return res.status(201).json(result)
+        return Response.json(goal);
     } catch (error) {
         console.error(error);
         return Response.error();
